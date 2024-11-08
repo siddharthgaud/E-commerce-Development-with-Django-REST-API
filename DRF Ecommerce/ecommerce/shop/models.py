fdf0deccc0a48a464bebaceebb89b1ffaaa8e20f
Siddharth Gaud <69710955+siddharthgaud@users.noreply.github.com>
2024-11-08 15:29:27 +0530

    Add files via upload

---
+++ DRF Ecommerce/ecommerce/shop/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order of {self.product.name} - Qty: {self.quantity}"
+
